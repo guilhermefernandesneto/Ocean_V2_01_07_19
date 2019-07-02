4d3e5fb4443d180ef8bed70153e4ef34db61e3e7
Paulo Salvatore <paulo@salvatore.tech>
2019-07-01 21:35:46 -0300

    Initial commit

---
+++ Assets/GeradorZumbis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorZumbis : MonoBehaviour {
+
+	public GameObject zumbiPrefab;
+	public float delay = 2f;
+	public float area = 3f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("GerarZumbi", delay, delay);
+	}
+
+	void GerarZumbi () {
+		var zumbi = Instantiate(zumbiPrefab);
+
+		var posicaoAleatoria = Random.insideUnitCircle * area;
+		zumbi.transform.position = new Vector3(
+			posicaoAleatoria.x,
+			zumbiPrefab.transform.position.y,
+			posicaoAleatoria.y
+		);
+	}
+}
---
+++ Assets/Zumbi.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Zumbi : MonoBehaviour {
+
+	Animator anim;
+	Rigidbody rb;
+	GameObject jogador;
+
+	public float velocidade = 0.35f;
+
+	bool morto = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody>();
+
+		jogador = GameObject.Find("Jogador");
+		transform.LookAt(jogador.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!morto)
+		{
+			rb.velocity = transform.forward * velocidade;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!morto && other.CompareTag("Projétil"))
+		{
+			morto = true;
+			Destroy(other.gameObject);
+			anim.SetTrigger("Morrer");
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+}
